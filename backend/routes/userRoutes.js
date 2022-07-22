6b709753fe54e8f8e531a09aba8e0743215964cb
Ali Falahi <ali@afalahi.com>
2022-07-22 17:11:09 -0600

    refactored routes to hadnle 405 errors. partial

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
+
 const {
   registerUser,
   loginUser,
   getMe,
 } = require('../controllers/userController');
 const protect = require('../middleware/authMiddleware');
+const AppError = require('../utils/AppError');
+
+router.get('/', (req, res) => {
+  res.json('Users');
+});
+
 router
-  .get('/', (req, res) => {
-    res.json('Users');
-  })
-  .post('/login', loginUser)
-  .post('/register', registerUser)
-  .get('/me', protect, getMe);
+  .route('/register')
+  .post(registerUser)
+  .all((req, res, next) => {
+    return next(new AppError('Method Not Allowed', 405));
+  });
+
+router
+  .route('/login')
+  .post(loginUser)
+  .all((req, res, next) => {
+    return next(new AppError('Method Not Allowed', 405));
+  });
+router
+  .route('/me')
+  .get(protect, getMe)
+  .all((req, res, next) => {
+    return next(new AppError('Method Not Allowed', 405));
+  });
 
 module.exports = router;
