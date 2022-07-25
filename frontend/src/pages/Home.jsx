9685fcd3325589dba67cf4e922c98471d92dbc16
Ali Falahi <ali@afalahi.com>
2022-07-24 21:23:54 -0600

    ticket ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
-import { Box, Container, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Container, VStack } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <Box>
-      <Container>
-        <Text>Home</Text>
-      </Container>
+      <Center>
+        <VStack>
+          <Box mb={5}>
+            <Button w={['30vh', '40vh', '50vh']} h={['5vh', '3vh', '5vh']}>
+              New Ticket
+            </Button>
+          </Box>
+          <Box>
+            <Button w={['30vh', '40vh', '50vh']} h={'5vh'}>
+              View Your Tickets
+            </Button>
+          </Box>
+        </VStack>
+      </Center>
     </Box>
   );
 };
