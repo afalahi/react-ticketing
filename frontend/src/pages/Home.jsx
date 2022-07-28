7bcdce01fd175b740b5fce9badb27225aef66057
Ali Falahi <ali@afalahi.com>
2022-07-27 23:16:32 -0400

    show buttons in middle

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
+/** @format */
+
 import { Box, Button, Center, Container, VStack } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <Box>
       <Center>
-        <VStack>
+        <VStack
+          spacing={5}
+          justifyContent='center'
+          justifySelf={'center'}
+          justifyItems='center'
+        >
+          <Box minH={'30vh'} />
           <Box mb={5}>
             <Button w={['30vh', '40vh', '50vh']} h={['5vh', '3vh', '5vh']}>
               New Ticket
